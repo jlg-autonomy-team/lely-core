c154acfaa7cf344b70238dfc64f7eaae01863ef0
J. S. Seldenthuis <jseldenthuis@lely.com>
2023-12-12 16:55:25 +0100

    release v2.3.3

--- python/can/lely_can/__init__.py
+++ python/can/lely_can/__init__.py
@@ -2,7 +2,7 @@
 
 __copyright__ = '2023 Lely Industries N.V.'
 __author__ = 'J. S. Seldenthuis <jseldenthuis@lely.com>'
-__version__ = '2.3.2'
+__version__ = '2.3.3'
 __license__ = 'Apache-2.0'
 
 from lely_can.err import *
